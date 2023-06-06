f6b970f7ad125c80e37d0b483e40b0c5d5b4e6cf
Hu <huang.h0709@gmail.com>
2023-06-06 15:35:31 -0400

    created person class

--- classFactoriesHW.js
+++ classFactoriesHW.js
@@ -16,3 +16,47 @@ class Hamster{
     }
 }
 
+class Person{
+    constructor(name,age,height,weight,mood,hamsters,bankAmount){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+    getName(){
+        return this.name;
+    }
+    getAge(){
+        return this.age;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    greet(){
+        console.log(this.name);
+    }
+    eat(){
+        weight++;
+        mood++;
+    }
+    exercise(){
+        weight--;
+    }
+    ageUp(){
+        age++;
+        height++;
+        weight++;
+        mood--;
+        bankAccount+=10;
+    }
+    buyHamster(){
+        hamsters.push(Hamster.name);
+        mood += 10;
+        bankAcocunt = bankAccount-getPrice();
+    }
+}
+
+

510c2444e1b82b6fca2542320c0389401dc6cdbc
Hu <huang.h0709@gmail.com>
2023-06-07 17:33:14 -0400

    continued working on Hamster/Person

--- classFactoriesHW.js
+++ classFactoriesHW.js
@@ -1,8 +1,8 @@
 //Hamster
 class Hamster{
-    constructor(owner,name,price){
-        this.owner = owner;
-        this.name = name;
+    constructor(){
+        this.owner = owner
+        this.hamsterName = hamsterName;
         this.price = price;
     }
     wheelRun(){
@@ -15,18 +15,20 @@ class Hamster{
         return this.price;
     }
 }
+const ham = new Hamster(owner="",hamsterName=this.hamsterName,price=15);
 class Person{
-    constructor(name,age,height,weight,mood,hamsters,bankAmount){
-        this.name = name;
+    constructor(personName,age=0,height=0,weight=0,mood=0,hamsters=[]
+        ,bankAccount=0){
+        this.personName = personName;
         this.age = age;
         this.height = height;
         this.weight = weight;
         this.mood = mood;
         this.hamsters = hamsters;
-        this.bankAmount = bankAmount;
+        this.bankAccount = bankAccount;
     }
     getName(){
-        return this.name;
+        return this.personName;
     }
     getAge(){
         return this.age;
@@ -35,15 +37,14 @@ class Person{
         return this.weight;
     }
     greet(){
-        console.log(this.name);
+        console.log(this.personName);
     }
-    eat(num){
+    eat(){
         weight++;
         mood++;
     }
-    exercise(n){
+    exercise(){
         weight--;
-        return n;
     }
     ageUp(){
         age++;
@@ -53,50 +54,49 @@ class Person{
         bankAccount+=10;
     }
     buyHamster(){
-        hamsters.push(Hamster.name);
-        mood += 10;
-        bankAcocunt = bankAccount-getPrice();
+        this.hamsters.push(ham.hamsterName);//?
+        this.mood += 10;
+        this.bankAcocunt = this.bankAccount-ham.getPrice();
     }
-    // eat(num){
-    //     return num;
-    // }
 }
 
 //Create a story with your Person class
-const peep = new Person("Timmy",5,60)
-peep.exercise = 5;
-// console.log(peep.exercise)
-console.log(peep.age)
-//hamster named Gus, cost $15, owner is Timmy:
-const ham = new Hamster("Timmy","Gus",15)
+//new Person instance 'Timmy'
+const timmy = new Person();
+timmy.personName = "Timmy";
+timmy.age += 5;
+timmy.eat = 5;
+timmy.exercise = 5;
+timmy.age += 9;
+timmy.buyHamster();
+ham.hamsterName = "Gus"
+console.log(timmy);
 console.log(ham)
-console.log(ham.price);
-//Have Timmy eat twice
-console.log(`Timmy ate ${peep.eat(2)} times`);
-//Have Timmy exercise twice
-console.log(`Timmy exercised ${peep.exercise(2)}`);
 
-//Chef Make Dinner
-class Dinner{
-    constructor(str,appetizer,entree,dessert){
-        this.str = str;
-        this.appetizer = appetizer;
-        this.entree = entree;
-        this.dessert = dessert;
-    }
-}
-class Chef{
-    constructor(ingrdient1,ingredient2,ingredient3){
-        // if(ingredient1 === "apple" &&)
+// //Chef Make Dinner
+// class Dinner{
+//     constructor(){
+//         this.str = str;
+//         this.appetizer = appetizer;
+//         this.entree = entree;
+//         this.dessert = dessert;
+//     }
+//     appet(){
+        
+//     }
+// }
+// class Chef{
+//     constructor(){
+//         this.
 
-    }
-    salad(){
-        console.log(`salad`);
-    }
-    fries(){
-        console.log(`fries`);
-    }
-    mixBowl(){
-        console.log(`mixBowl`)
-    }
-}
+//     }
+//     salad(){
+        
+//     }
+//     fries(){
+//         console.log(`fries`);
+//     }
+//     mixBowl(){
+//         console.log(`mixBowl`)
+//     }
+// }
